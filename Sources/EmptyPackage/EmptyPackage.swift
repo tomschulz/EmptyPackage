60a446db984f6537b786aa1209158309f285688d
Tom Schulz <tom@silverorange.com>
2025-04-06 17:08:58 -0700

    First Commit

---
+++ Sources/EmptyPackage/EmptyPackage.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+class EmptyPackageBuddyFriendGuy {
+    
+    let buddy = "Friend"
+    let friend = "Guy"
+    let guy = "Buddy"
+    
+    func sayHello() -> String {
+        return "Hello \(buddy). Want to be me \(guy), \(friend)?"
+    }
+    
+}
