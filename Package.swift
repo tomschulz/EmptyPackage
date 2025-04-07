e3232ec1f8bb7ac64d90427f2237eee95976e897
Tom Schulz <tom@silverorange.com>
2025-04-06 17:47:06 -0700

    Making package that uses Cocoalumberjack

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -11,11 +10,14 @@ let package = Package(
             name: "EmptyPackage",
             targets: ["EmptyPackage"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack", from: "3.6.2")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "EmptyPackage"),
-
+            name: "EmptyPackage",
+            dependencies: [
+                .product(name: "CocoaLumberjackSwift", package: "CocoaLumberjack")
+            ])
     ]
 )
--- Sources/EmptyPackage/EmptyPackage.swift
+++ Sources/EmptyPackage/EmptyPackage.swift
@@ -1,6 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import CocoaLumberjackSwift
+
 class EmptyPackageBuddyFriendGuy {
     
     let buddy = "Friend"
